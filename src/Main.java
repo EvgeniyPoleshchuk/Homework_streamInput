ca69d058ef2eadd24031015584879d32d7467e4f
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-03-24 10:58:26 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 
 public class Main extends CreateDirAndFiles {
     public static void main(String[] args) {
